f27c0ea38513f5189e4feb33694fb8c4b41920e9
rdeepakp <43665380+rdeepakp@users.noreply.github.com>
2018-09-28 13:10:52 +0530

    Create day 1.c

---
+++ day 1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+ int a[10];
+ printf("\n hello world");
+ }
+ Answer 2
+
+#include<stdio.h>
+void main()
+{
+ char a[10];
+printf("\n enter your name:");
+scanf("%s",a);
+
+printf("%s",a);
+}
+
+What is #include <stdio.h>?
+ It is header file used to read and write input and output 
+From the user.
+
+What is printf?
+It is used to print the character,float,integer,string,octal and 
+Hexadecimal values.
+
+What is main()?
+It defines a function that takes no argument.
+
+
+Why should you learn touch typing on a qwerty keyboard?
+Type the above sentence as it is,by using shift keys to big alphabets.
